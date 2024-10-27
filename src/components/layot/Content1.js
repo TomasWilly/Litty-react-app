d0d4090335dbfcc2139eadce6905d67b9a304983
Yil-Verdeja <codecooker492@gmail.com>
2024-10-27 08:55:42 -0700

    Complete one page

--- src/components/layot/Content1.js
+++ src/components/layot/Content1.js
@@ -8,8 +8,7 @@ const Content1 = (props) => {
             <h3 className='font-normal text-[14px]'>{props.data.name}</h3>
             {props.data.key1?<button className='font-normal text-[12px] place-self-center'>GAMING</button>:<></>}
             {props.data.key2?<button className='font-normal text-[12px]'>TECH</button>:<></>}
-        </div>
-        
+        </div>        
         <h4 className='flex-none text-[18px] font-normal'>{props.data.value}K</h4>
         <img className='flex-none w-[18.24px] h-[16px] mr-[15px] ml-[12px]' src="assets/images/logo.png" alt="" />
     </div>
